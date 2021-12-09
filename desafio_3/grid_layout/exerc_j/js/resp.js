fd49e62806a623377949ace085517d2b3e62b2d7
Lara Correa Joaquim <lcorreaj@emeal.nttdata.com>
2021-12-08 22:27:24 -0300

    inclusão jquery grid

--- desafio_3/grid_layout/exerc_j/js/resp.js
+++ desafio_3/grid_layout/exerc_j/js/resp.js
@@ -1,15 +1,25 @@
-$('#navIt').ready(function(){
-    $("img").click(function(){
+function toggleHorizontalMenu(){
+    if($(document).innerWidth() < 800) {
+        //diminui os elementos, esconde se for necessário
+        $("#btn_menu").show();
+        $("#menu_list").addClass("hamburger_menu");
+        $("#menu_list").removeClass("horizontal_menu");
+        $("#menu_list").hide();
+    } else {
+        //mantem um elemento ou outro
+        $("#btn_menu").hide();
+        $("#menu_list").removeClass("hamburger_menu");
+        $("#menu_list").addClass("horizontal_menu");
+        $("#menu_list").show();
+    }
+} 
+$(document).ready(function(){
+    toggleHorizontalMenu();
+    $("#btn_menu").click(function(){
+        $("#menu_list").toggle();
         //deve mostrar o menu em forma de lista quando em tela menor
-        $("ul").toggle('slow');
     })
 });
 $(window).resize(function(){
-    if($('#navIt').innerWidth() < 600) {
-        //diminui os elementos, esconde se for necessário
-        $(this).addClass('drop');
-    } else {
-        //mantem um elemento ou outro
-        $(this).removeClass('drop');
-    }
+   toggleHorizontalMenu();
 });
