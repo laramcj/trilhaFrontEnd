16a4c651e17f78c12607a0478d05006626c34b4d
laramcj <lara.mcj@gmail.com>
2021-12-16 14:55:59 -0300

    desafio 5.1 finalizado

---
+++ desafio_5.1/projeto-desafio/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'projeto-desafio';
+  nome = 'Lara Maia Corrêa Joaquim';
+  guia = 'Elivel Santos Nascimento';
+  dataDesafio = 'Quinta 15 de dezembro de 2021 14:31:05 GMT-0300';
+  numeroTurma = 'Turma Cybertron - Só Vamu!';
+}
+
